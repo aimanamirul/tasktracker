94e413fc84a4b2f4e94b5ecd1c22dd9dbdb96150
aimanamirul <aimanamirul2501@gmail.com>
2020-05-21 14:36:08 +0800

    initial commit

---
+++ src/Components/TaskRow/TaskRow.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+
+class TaskRow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default TaskRow;
